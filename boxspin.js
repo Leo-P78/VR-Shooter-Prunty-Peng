26503e1613dbb1a9a45a790907ebeb64c7c4399b
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-07 14:33:11 -0800

    Add box-spin component for rotating boxes

---
+++ boxspin.js
@@ -0,0 +1,12 @@
+/* global AFRAME, BOX_SPEED */
+
+AFRAME.registerComponent('box-spin', {
+
+  tick: function (time, delta) {
+    let rotation = this.el.getAttribute('rotation')
+    rotation.y = (rotation.y + BOX_SPEED) % 360
+    this.el.setAttribute('rotation', rotation)
+  },
+  
+});
+
