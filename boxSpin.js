f5cc49847b49c482966d011cfb3b8de345890acd
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-10 10:53:25 -0800

    Update boxSpin.js

--- boxSpin.js
+++ boxSpin.js
@@ -1,10 +1,8 @@
-/* global AFRAME, BOX_SPEED */
-
 AFRAME.registerComponent('box-spin', {
 
   tick: function (time, delta) {
     let rotation = this.el.getAttribute('rotation')
-    rotation.y = (rotation.y + BOX_SPEED) % 360
+    rotation.y = (rotation.y + 1) % 360
     this.el.setAttribute('rotation', rotation)
   },
   
