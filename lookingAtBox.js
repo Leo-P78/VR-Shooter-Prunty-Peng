eee52fe0349b363befbbdfee8d823d44b0844bdc
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-10 11:26:03 -0800

    Update lookingAtBox.js

--- lookingAtBox.js
+++ lookingAtBox.js
@@ -1,15 +1,18 @@
-/* global AFRAME */
+/* global AFRAME, BOX_SELECTED_COLOR */
 
 AFRAME.registerComponent('looking-at-box', {
+
  dependencies: ['raycaster'],
 
  init: function () {
    this.el.addEventListener('raycaster-intersected', this.intersected.bind(this))
  },
-
+  
  intersected: function () {
-   console.log('Raycaster hit the box!');
+   let material = this.el.getAttribute('material')
+   material.color = BOX_SELECTED_COLOR
+   this.el.setAttribute('material', material)
  }
-
+  
 });
 
