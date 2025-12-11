05d4e13780755e7d0b27d3ea946b93b46ae7c0d5
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-10 19:24:28 -0800

    Update clickOnBox.js

--- clickOnBox.js
+++ clickOnBox.js
@@ -5,17 +5,11 @@ AFRAME.registerComponent('click-on-box', {
   init: function () {
     this.el.addEventListener('click', this.boxClicked.bind(this));
   },
-  function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min; 
-    }
-
   boxClicked: function() {
     this.el.setAttribute('position', {
-        x: getRandomInt(-10,10),
-        y: getRandomInt(1,10),
-        z: getRandomInt(-2,-10)
+        x: parseInt(Math.random() * (10 - (-10)) + (-10), 10),
+        y: parseInt(Math.random() * (1 - 10) + (-10), 10),
+        z: parseInt(Math.random() * (-2 - (-10)) + (-10), 10)
     });
   }
 });
