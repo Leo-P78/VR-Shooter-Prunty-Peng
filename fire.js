5a3244920c3ac69c7c1a12f5b8148b95fcdb7027
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-10 20:27:54 -0800

    Update fire.js

--- fire.js
+++ fire.js
@@ -7,7 +7,9 @@ AFRAME.registerComponent('fire', {
  },
 
  fire: function() {
-   console.log('fired')
- }
+   if (!this.el.getAttribute('visible')) {
+  this.el.setAttribute('visible', true);
+  this.el.setAttribute('position', {x:0, y:1, z:-1});
+}
 });
 
