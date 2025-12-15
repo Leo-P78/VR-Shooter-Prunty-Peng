70b56efb1226d4a03504ef825c3b5af775e193c9
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-14 17:47:16 -0800

    Update clickOnBox.js

--- clickOnBox.js
+++ clickOnBox.js
@@ -5,18 +5,28 @@ AFRAME.registerComponent('click-on-box', {
   init: function () {
     this.el.addEventListener('click', this.boxClicked.bind(this));
     this.raycaster = null;
+    console.log('click-on-box component initialized on:', this.el);
   },
 
   boxClicked: function() {
+    console.log('CLICK EVENT FIRED!');
+    
     // Get the raycaster from the parent entity
     if (!this.raycaster) {
+      console.log('Getting raycaster from parent');
       this.raycaster = this.el.parentNode.components.raycaster;
+      console.log('Raycaster:', this.raycaster);
+    }
+    
+    if (!this.raycaster) {
+      console.log('ERROR: No raycaster found!');
+      return;
     }
     
     // Get what the raycaster is currently intersecting
     let intersections = this.raycaster.intersectedEls;
     
-    console.log('Click! Intersections:', intersections);
+    console.log('Intersections:', intersections);
     
     if (intersections && intersections.length > 0) {
       // Get the first intersected element (the box)
@@ -27,6 +37,8 @@ AFRAME.registerComponent('click-on-box', {
       
       let torpedo = document.getElementById('torpedo');
       torpedo.emit('fire', {boxPosition: boxPosition});
+    } else {
+      console.log('No intersections found!');
     }
   }
 });
