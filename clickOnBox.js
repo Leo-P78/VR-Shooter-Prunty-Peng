f1bb7492be3f9bff8d21e94c572936949ee42089
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-14 17:45:48 -0800

    Update clickOnBox.js

--- clickOnBox.js
+++ clickOnBox.js
@@ -4,15 +4,29 @@ AFRAME.registerComponent('click-on-box', {
 
   init: function () {
     this.el.addEventListener('click', this.boxClicked.bind(this));
+    this.raycaster = null;
   },
-  
+
   boxClicked: function() {
-    // this.el is now the box itself
-    let boxPosition = this.el.getAttribute('position');
+    // Get the raycaster from the parent entity
+    if (!this.raycaster) {
+      this.raycaster = this.el.parentNode.components.raycaster;
+    }
     
-    console.log('Box clicked! Position:', boxPosition);
+    // Get what the raycaster is currently intersecting
+    let intersections = this.raycaster.intersectedEls;
     
-    let torpedo = document.getElementById('torpedo');
-    torpedo.emit('fire', {boxPosition: boxPosition});
+    console.log('Click! Intersections:', intersections);
+    
+    if (intersections && intersections.length > 0) {
+      // Get the first intersected element (the box)
+      let box = intersections[0];
+      let boxPosition = box.getAttribute('position');
+      
+      console.log('Box found at position:', boxPosition);
+      
+      let torpedo = document.getElementById('torpedo');
+      torpedo.emit('fire', {boxPosition: boxPosition});
+    }
   }
 });
