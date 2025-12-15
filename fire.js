7a86d808aac8fea08d347022cff9dc763ffef96a
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-14 17:37:42 -0800

    Update fire.js

--- fire.js
+++ fire.js
@@ -8,17 +8,26 @@ AFRAME.registerComponent('fire', {
 
  init: function () {
    this.el.addEventListener('fire', this.fire.bind(this));
+   console.log('Fire component initialized');
  },
 
  fire: function(event) {
+   console.log('Fire event received');
+   console.log('Torpedo visible:', this.el.getAttribute('visible'));
+   console.log('Event detail:', event.detail);
+   
    if (!this.el.getAttribute('visible')) {
      let camera = document.getElementById('camera');
      let cameraPos = camera.getAttribute('position');
      let boxPos = event.detail.boxPosition;
      
+     console.log('Camera:', camera);
+     console.log('Camera pos:', cameraPos);
+     console.log('Box pos:', boxPos);
+     
      // Calculate distance from camera to box
      this.data.boxDistance = this.getDistance(cameraPos, boxPos);
-     console.log('Firing torpedo! Camera pos:', cameraPos, 'Box pos:', boxPos, 'Distance:', this.data.boxDistance);
+     console.log('Calculated distance:', this.data.boxDistance);
      
      this.el.setAttribute('visible', true);
      this.el.setAttribute('position', {x: 0, y: 0, z: -1});
@@ -26,4 +35,28 @@ AFRAME.registerComponent('fire', {
  },
 
  tick: function(time, timeDelta) {
-   if (this.el.getAttribute('
+   if (this.el.getAttribute('visible')) {
+     let pos = this.el.getAttribute('position')
+     let torpedoDx = this.getDx(pos)
+     
+     if (torpedoDx > this.data.boxDistance + TORPEDO_MARGIN) {
+       this.el.setAttribute('visible', false)
+     } else {
+       pos.z += -timeDelta * TORPEDO_SPEED / 1000.0;
+       this.el.setAttribute('position', pos)
+     }
+   }
+ },
+
+ getDx: function(vec3) {
+   return Math.sqrt(vec3.x*vec3.x + vec3.y*vec3.y + vec3.z*vec3.z)
+ },
+ 
+ getDistance: function(pos1, pos2) {
+   let dx = pos2.x - pos1.x;
+   let dy = pos2.y - pos1.y;
+   let dz = pos2.z - pos1.z;
+   return Math.sqrt(dx*dx + dy*dy + dz*dz);
+ }
+
+});
