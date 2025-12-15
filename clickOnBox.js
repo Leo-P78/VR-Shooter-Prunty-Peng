d6815f8ad48711d39be0ea8039ab339508666f51
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-14 17:40:07 -0800

    Update clickOnBox.js

--- clickOnBox.js
+++ clickOnBox.js
@@ -5,12 +5,18 @@ AFRAME.registerComponent('click-on-box', {
   init: function () {
     this.el.addEventListener('click', this.boxClicked.bind(this));
   },
-  boxClicked: function() {
-  // Get a reference to the torpedo by looking at the id value
-  // of the HTML element and emit the 'fire' event
-  let torpedo = document.getElementById('torpedo');
-  let boxPosition = this.el.getAttribute('position')
-  torpedo.emit('fire', {boxPosition: boxPosition})
-}
+  
+  boxClicked: function(event) {
+    // Get the box that was actually clicked using the raycaster intersection
+    let intersectedEl = event.detail.intersection ? event.detail.intersection.object.el : null;
+    
+    // If we have an intersected element, use its position, otherwise fall back to getting box by ID
+    let box = intersectedEl || document.getElementById('box');
+    let boxPosition = box.getAttribute('position');
+    
+    console.log('Click detected! Box position:', boxPosition);
+    
+    let torpedo = document.getElementById('torpedo');
+    torpedo.emit('fire', {boxPosition: boxPosition});
+  }
 });
-
