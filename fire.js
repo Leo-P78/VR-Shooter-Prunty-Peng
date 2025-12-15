bdb47f3b4c9a9c3c8517417630e09eb6cb05723a
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-14 17:57:49 -0800

    Update fire.js

--- fire.js
+++ fire.js
@@ -12,13 +12,7 @@ AFRAME.registerComponent('fire', {
 
  fire: function(event) {
    if (!this.el.getAttribute('visible')) {
-     let camera = document.getElementById('camera');
-     let cameraPos = camera.getAttribute('position');
-     let boxPos = event.detail.boxPosition;
-     
-     this.data.boxDistance = this.getDistance(cameraPos, boxPos);
-     console.log('FIRING! Box at:', boxPos, 'Distance:', this.data.boxDistance);
-     
+     this.data.boxDistance = this.getDx(event.detail.boxPosition);
      this.el.setAttribute('visible', true);
      this.el.setAttribute('position', {x: 0, y: 0, z: -1});
    }
@@ -26,15 +20,19 @@ AFRAME.registerComponent('fire', {
 
  tick: function(time, timeDelta) {
    if (this.el.getAttribute('visible')) {
-     // Get world position of torpedo
-     let worldPos = new THREE.Vector3();
-     this.el.object3D.getWorldPosition(worldPos);
-     
-     // Get camera world position
-     let camera = document.getElementById('camera');
-     let cameraWorldPos = new THREE.Vector3();
-     camera.object3D.getWorldPosition(cameraWorldPos);
-     
-     // Calculate distance traveled from camera
-     let distanceTraveled = this.getDistance(
-       {x: cameraWorldPos.x, y:
+     let pos = this.el.getAttribute('position')
+     let torpedoDx = this.getDx(pos)
+     if (torpedoDx > this.data.boxDistance + TORPEDO_MARGIN) {
+       this.el.setAttribute('visible', false)
+     } else {
+       pos.z += -timeDelta * TORPEDO_SPEED / 1000.0;
+       this.el.setAttribute('position', pos)
+     }
+   }
+ },
+
+ getDx: function(vec3) {
+   return Math.sqrt(vec3.x*vec3.x + vec3.y*vec3.y + vec3.z*vec3.z)
+ }
+
+});
