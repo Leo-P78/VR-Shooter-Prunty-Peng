a7672be912a8221b8794e0edc860136c19159113
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-14 18:01:04 -0800

    Update collide.js

--- collide.js
+++ collide.js
@@ -8,21 +8,33 @@ AFRAME.registerComponent('collide', {
 
  init: function () {
    this.el.addEventListener('hitstart', this.collide.bind(this));
+   this.el.addEventListener('hitend', function() {
+     console.log('Collision ended');
+   });
    console.log('Collide component initialized');
  },
 
  collide: function() {
    console.log('COLLISION DETECTED!');
+   console.log('Torpedo position:', this.el.getAttribute('position'));
+   console.log('Torpedo world position:', this.el.object3D.getWorldPosition(new THREE.Vector3()));
+   
    let box = document.getElementById('box');
-   let position = box.getAttribute('position');
-   console.log('Old box position:', position);
-   position.x = Math.random() * 10 * ((Math.random() < 0.5) ? -1 : 1);
-   position.y = Math.random() * 9 + 1;
-   position.z = -(Math.random() * 8 + 2);
+   let oldPosition = box.getAttribute('position');
+   console.log('Old box position:', oldPosition);
+   
+   let position = {
+     x: Math.random() * 10 * ((Math.random() < 0.5) ? -1 : 1),
+     y: Math.random() * 9 + 1,
+     z: -(Math.random() * 8 + 2)
+   };
+   
    box.setAttribute('position', position);
    console.log('New box position:', position);
+   
    this.el.setAttribute('position', {x: 0, y: 0, z: -1});
    this.el.setAttribute('visible', false);
+   console.log('Torpedo reset and hidden');
  },
 
 });
