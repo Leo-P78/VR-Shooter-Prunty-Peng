df6e373f304a4a492be39567050bc1373e20ed71
Leo-P78 <lp4882@pleasantonusd.net>
2025-12-14 17:31:08 -0800

    Update fire.js

--- fire.js
+++ fire.js
@@ -8,13 +8,18 @@ AFRAME.registerComponent('fire', {
 
  init: function () {
    this.el.addEventListener('fire', this.fire.bind(this));
+   console.log('Fire component initialized');
  },
 
  fire: function(event) {
+   console.log('Fire event received, torpedo visible:', this.el.getAttribute('visible'));
    if (!this.el.getAttribute('visible')) {
      this.data.boxDistance = this.getDx(event.detail.boxPosition);
+     console.log('Firing torpedo! Box distance:', this.data.boxDistance);
      this.el.setAttribute('visible', true);
      this.el.setAttribute('position', {x: 0, y: 0, z: -1});
+   } else {
+     console.log('Torpedo already visible, not firing');
    }
  },
 
@@ -24,6 +29,7 @@ AFRAME.registerComponent('fire', {
      let torpedoDx = this.getDx(pos)
      if (torpedoDx > this.data.boxDistance + TORPEDO_MARGIN) {
        this.el.setAttribute('visible', false)
+       console.log('Torpedo went too far, hiding');
      } else {
        pos.z += -timeDelta * TORPEDO_SPEED / 1000.0;
        this.el.setAttribute('position', pos)
@@ -36,4 +42,3 @@ AFRAME.registerComponent('fire', {
  }
 
 });
-
